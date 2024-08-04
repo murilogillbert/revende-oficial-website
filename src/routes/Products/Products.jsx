ad742ac736a732f6b27789f079d11664278db31c
murilogillbert <murilo.gillbert@outlook.com>
2024-08-03 22:31:55 -0400

    Modificação em products para redirecionar para a página do produto

--- src/routes/Products/Products.jsx
+++ src/routes/Products/Products.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react';
 import './Products.css';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
+import { useNavigate } from 'react-router-dom';
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch('src/data/products.json')
@@ -13,8 +15,11 @@ const Products = () => {
       .catch((error) => console.error('Error fetching products:', error));
   }, []);
 
-  return (
+  const handleClick = (productId) => {
+    navigate(`/produtos/${productId}`);
+  };
 
+  return (
     <div className="produtos">
       <header className="navbar">
         <nav>
@@ -27,10 +32,15 @@ const Products = () => {
           <h2>{category.category}</h2>
           <div className="product-list">
             {category.products.map((product, index) => (
-              <div key={index} className="product-card">
+              <div
+                key={index}
+                className="product-card"
+                onClick={() => handleClick(product.id)}
+              >
                 <img src={product.img} alt={product.name} />
                 <h3>{product.name}</h3>
                 <p>{product.description}</p>
+                <span>R$ 100</span>
               </div>
             ))}
           </div>
